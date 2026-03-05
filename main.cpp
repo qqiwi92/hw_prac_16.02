1823d31167260b90145cd647e3c7a595481ea4e6
Dima <mit.levkin@vk.com>
2026-03-05 23:16:46 +0300

    add usage in main()

--- main.cpp
+++ main.cpp
@@ -7,6 +7,30 @@ T * merge(const T * const * a, size_t sa, const size_t * sai, T * c ) {
   return c;
 }
 
-int main () {
-  std::cout << "levkin.dima\n";
+int main() {
+    int arr1[] = {10, 20, 30};
+    int arr2[] = {5, 15, 25, 45};
+    int arr3[] = {1, 2, 100};
+
+    const int* const a[] = {arr1, arr2, arr3};
+
+    size_t sai[] = {3, 4, 3};
+
+    size_t sa = 3;
+
+    size_t total_size = 0;
+    for (size_t i = 0; i < sa; ++i) total_size += sai[i];
+    
+    int* result = new int[total_size];
+
+    merge(a, sa, sai, result);
+
+    for (size_t i = 0; i < total_size; ++i) {
+        std::cout << result[i] << " ";
+    }
+    std::cout << "\n";
+
+    delete[] result;
+
+    return 0;
 }
