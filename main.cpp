4b4e6aa2a49648a371016e651a5c825d2bc06f0f
Dima <mit.levkin@vk.com>
2026-03-05 23:38:37 +0300

    here wanted eat the first element by incrementing start, but since it is const we can't do that

--- main.cpp
+++ main.cpp
@@ -1,36 +1,52 @@
 #include <iostream>
 
+template <class T>
+T *merge(const T *const *a, size_t sa, const size_t *sai, T *c) {
 
+  size_t c_i = 0;
 
-template <class T> 
-T * merge(const T * const * a, size_t sa, const size_t * sai, T * c ) {
+  for (size_t i = 0; i < sa; ++i) {
+    for (size_t j = 0; j < sai[i]; ++j) {
+      T minn = a[0][0];
+      for (size_t fi = 1; i < sa; ++i) {
+        minn = a[fi][0] < minn ? a[fi][0] : minn; 
+      }
+      for (size_t fi = 0; i < sa; ++i) {
+        if (a[fi][0] == minn){
+          a[fi][0] = a[fi][0] + 1;
+          break;
+        }
+      }
+    }
+  }
   return c;
 }
 
 int main() {
-    int arr1[] = {10, 20, 30};
-    int arr2[] = {5, 15, 25, 45};
-    int arr3[] = {1, 2, 100};
+  int arr1[] = {10, 20, 30};
+  int arr2[] = {5, 15, 25, 45};
+  int arr3[] = {1, 2, 100};
 
-    const int* const a[] = {arr1, arr2, arr3};
+  const int *const a[] = {arr1, arr2, arr3};
 
-    size_t sai[] = {3, 4, 3};
+  size_t sai[] = {3, 4, 3};
 
-    size_t sa = 3;
+  size_t sa = 3;
 
-    size_t total_size = 0;
-    for (size_t i = 0; i < sa; ++i) total_size += sai[i];
-    
-    int* result = new int[total_size];
+  size_t total_size = 0;
+  for (size_t i = 0; i < sa; ++i)
+    total_size += sai[i];
 
-    merge(a, sa, sai, result);
+  int *result = new int[total_size];
 
-    for (size_t i = 0; i < total_size; ++i) {
-        std::cout << result[i] << " ";
-    }
-    std::cout << "\n";
+  merge(a, sa, sai, result);
 
-    delete[] result;
+  for (size_t i = 0; i < total_size; ++i) {
+    std::cout << result[i] << " ";
+  }
+  std::cout << "\n";
 
-    return 0;
+  delete[] result;
+
+  return 0;
 }
