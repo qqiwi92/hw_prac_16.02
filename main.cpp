532ba7afad78ed009207785ab9bd9a424f208e75
Dima <mit.levkin@vk.com>
2026-03-05 23:14:38 +0300

    add signatures

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+
+template <class T> 
+T * merge(const T * const * a, size_t sa, const size_t * sai, T * c ) {
+  return c;
+}
+
+int main () {
+  std::cout << "levkin.dima\n";
+}
